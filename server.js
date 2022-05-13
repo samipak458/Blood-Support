f856b80511018ad8a5cf315940012fa525fdd0d4
Shehroz Ali <sshehroza13@gmail.com>
2022-05-13 14:50:13 +0500

    Configured express to serve al HTML,CSS,JS, etc. files from public folder using static() default middleware

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Sever started");
+// Require Packages
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Express app config
+const publicPath = path.join(__dirname, "public"); // Get the path of 'public' folder
+app.use(express.static(publicPath)); // Use express default middleware to serve all HTML, CSS and JS files
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200); // Respond '200' OK
+});
+
+// Server will listen to PORT 3000
+app.listen(3000);
